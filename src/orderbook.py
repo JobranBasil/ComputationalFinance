0d1c1383a6a002d96eefe81a34920b5041b72349
BigBoyBiba <120318336+BigBoyBiba@users.noreply.github.com>
2026-02-20 13:54:11 +0000

    Update orderbook.py

--- src/orderbook.py
+++ src/orderbook.py
@@ -1,1 +1,10 @@
-# 
+#Order Book Class
+'''
+Need to create: 
+Order class with main features of order book, price, volume, etc.. 
+OrderBook with all bids and asks with volumes.
+Add function to allow traders to add limit order
+Add function to create matching, when there is a bid that matches the ask, match them but based on what rules? Double auction, price time priority, research rules online.
+Add function to market buys and market sells? Something like that.
+'''
+
